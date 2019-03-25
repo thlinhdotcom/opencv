1c50869f3ca707ea504b8e333933997644befd28
linhth <linhth@apecsoft.asia>
2019-03-25 22:58:48 +0700

    phần 2

---
+++ phan2/bai1.py
@@ -0,0 +1,9 @@
+﻿import cv2
+#thư viện xử lý tính toán
+import numpy as np
+img = cv2.imread('opencv.jpg');
+#cắt ảnh từ điểm 0,500 đến 400,500
+imgcrop = img[0:500, 400:500];
+cv2.imshow('image', imgcrop);
+cv2.waitKey(0);
+cv2.destroyAllWindows();
---
+++ phan2/bai2.py
@@ -0,0 +1,11 @@
+﻿import cv2
+import numpy as np
+img1 = cv2.imread('eye.jpg');
+img2 = cv2.imread('eye2.jpg');
+#cắt ảnh từ điểm 0,500 đến 400,500
+imgcrop1 = img1[100:300, 100:400];
+imgcrop2 = img2[100:300, 100:400];
+#ghép 2 ảnh vào nhau
+img3 = cv2.add(imgcrop1,imgcrop2);
+cv2.imshow('image', img3);
+cv2.waitKey(0);
