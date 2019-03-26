84575d527d787ac10296ba616fdfc34a5e932e30
linhth <linhth@apecsoft.asia>
2019-03-26 22:53:01 +0700

    thêm bài 3 phần 2

---
+++ phan2/bai3.py
@@ -0,0 +1,11 @@
+﻿import cv2
+import numpy as np
+img = cv2.imread('eye.jpg');
+print 'chieu rong';
+print img.shape[0];
+print 'chieu cao';
+print img.shape[1];
+#resize ảnh
+img_resize = cv2.resize(src=img, dsize=(300, 300));
+new_img = 'eye_new.jpg';
+cv2.imwrite(new_img, img_resize);
