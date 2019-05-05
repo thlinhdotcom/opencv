2af1eb49ba5dac1ec49bb19b209e777d4870712d
linhth <linhth@apecsoft.asia>
2019-05-05 23:51:21 +0700

    phần 3

---
+++ phan3/bai3.py
@@ -0,0 +1,13 @@
+﻿import cv2
+#lấy hình ảnh từ stream
+cap = cv2.VideoCapture('rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov')
+
+while(True):
+    ret, frame = cap.read()
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
