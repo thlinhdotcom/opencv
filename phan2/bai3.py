e22103542e910e75f58221e288c47faf6e63b107
LinhTH <linhth@apecsoft.asia>
2020-01-20 17:33:31 +0700

    fix python 3

--- phan2/bai3.py
+++ phan2/bai3.py
@@ -1,10 +1,10 @@
 ﻿import cv2
 import numpy as np
 img = cv2.imread('eye.jpg');
-print 'chieu rong';
-print img.shape[0];
-print 'chieu cao';
-print img.shape[1];
+print('chieu rong');
+print(img.shape[0]);
+print('chieu cao');
+print(img.shape[1]);
 #resize ảnh
 img_resize = cv2.resize(src=img, dsize=(300, 300));
 new_img = 'eye_new.jpg';
